369f131b96cf59edcc0213a5295002a16b8f8739
David Pello <dvpg@tuta.io>
2025-05-07 13:47:18 +0200

    Reading test

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+
+	ep9000 "github.com/ladecadence/EP9000.git/pkg/EP9000"
+)
+
+func main() {
+	data := make(chan []uint8)
+	scanner, err := ep9000.New("/dev/ttyACM0", 115200)
+	if err != nil {
+		panic(err)
+	}
+	go scanner.Listen(data)
+
+	select {
+	case recv := <-data:
+		fmt.Printf("Data: %s\n", recv)
+	}
+}
