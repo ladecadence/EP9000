f2d535206ddd47486cc0a8d5bb3e77cf1a3e6385
David Pello <dvpg@tuta.io>
2025-05-13 09:58:50 +0200

    Convert to module

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	ep9000 "github.com/ladecadence/EP9000/pkg/EP9000"
+)
+
+func main() {
+	data := make(chan []uint8)
+	scanner, err := ep9000.New("/dev/ttyACM1", 115200)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		err := scanner.Listen(data)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	for {
+		select {
+		case recv := <-data:
+			fmt.Printf("Data: %s\n", recv)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ladecadence/EP9000
+go 1.24.2
+
+require go.bug.st/serial v1.6.4
+
+require (
+	github.com/creack/goselect v0.1.2 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
